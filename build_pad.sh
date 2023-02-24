ed001677ad63947a8df038b9f3b8489a1cbf575f
jeanboy <jeanboy@foxmail.com>
2023-02-24 15:57:36 +0800

    update

---
+++ build_pad.sh
@@ -0,0 +1,4 @@
+#./gradlew :updateVersions
+./gradlew :module_test:clean :module_test:assemblePadRelease :module_test:uploadArchives --info
+
+
---
+++ build_phone.sh
@@ -0,0 +1,4 @@
+#./gradlew :updateVersions
+./gradlew :module_test:clean :module_test:assemblePhoneRelease :module_test:uploadArchives --info
+
+
